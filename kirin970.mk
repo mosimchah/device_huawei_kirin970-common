d12c23029325d26104f01d990c936201e8cbe80f
mosimchah <mosimchah@gmail.com>
2018-12-14 22:00:36 -0500

    kirin970-common: AICP-ify
    Change-Id: Ic50f4deb9a3eed6e7817577c6d4362a0cd552178

--- kirin970.mk
+++ kirin970.mk
@@ -18,8 +18,7 @@ $(call inherit-product, vendor/huawei/kirin970-common/kirin970-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
